23ed00fdc817b09d0dac05c868b919f997315ec2
Minh Luu <mluu510@gmail.com>
2014-04-13 14:03:28 -0700

    Can scroll now!

--- Top Places/PhotoViewController.m
+++ Top Places/PhotoViewController.m
@@ -9,10 +9,10 @@
 #import "PhotoViewController.h"
 #import "FlickrFetcher.h"
 
-@interface PhotoViewController ()
+@interface PhotoViewController () <UIScrollViewDelegate>
 
-@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
-@property (nonatomic, strong) UIActivityIndicatorView *spinner;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -21,8 +21,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
     self.title = self.photo[@"title"];
+    self.scrollView.delegate = self;
     [self downloadImage];
 }
 
@@ -30,15 +30,58 @@
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
     dispatch_queue_t downloadQueue = dispatch_queue_create("Image Download", NULL);
     dispatch_async(downloadQueue, ^{
+        [self.spinner startAnimating];
         NSURL *photoURL = [FlickrFetcher URLforPhoto:self.photo format:FlickrPhotoFormatLarge];
         NSData *photoData = [NSData dataWithContentsOfURL:photoURL];
         [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
         UIImage *photo = [UIImage imageWithData:photoData];
         dispatch_async(dispatch_get_main_queue(), ^{
-            self.photoImageView.image = photo;
+            [self displayImage:photo];
             [self.spinner stopAnimating];
         });
     });
+}
+
+- (void)displayImage:(UIImage *)image {
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    float ratio = MIN(self.scrollView.bounds.size.width / image.size.width, self.scrollView.bounds.size.height / image.size.height);
+    imageView.frame = CGRectMake(0, 0, imageView.frame.size.width * ratio, imageView.frame.size.height * ratio);
+    [self.scrollView addSubview:imageView];
+    self.scrollView.contentSize = imageView.frame.size;
+    self.scrollView.minimumZoomScale = 1;
+    self.scrollView.maximumZoomScale = 1/ratio;
+    [self centerScrollViewContent:self.scrollView];
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return [scrollView.subviews firstObject];
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
+    UIView *subView = [scrollView.subviews firstObject];
+    
+    CGFloat offsetX = (scrollView.bounds.size.width > scrollView.contentSize.width)?
+    (scrollView.bounds.size.width - scrollView.contentSize.width) * 0.5 : 0.0;
+    
+    CGFloat offsetY = (scrollView.bounds.size.height > scrollView.contentSize.height)?
+    (scrollView.bounds.size.height - scrollView.contentSize.height) * 0.5 : 0.0;
+    
+    subView.center = CGPointMake(scrollView.contentSize.width * 0.5 + offsetX,
+                                 scrollView.contentSize.height * 0.5 + offsetY);
+}
+
+- (void)centerScrollViewContent:(UIScrollView *)scrollView {
+    UIView *subView = [scrollView.subviews firstObject];
+    
+    CGFloat offsetX = (scrollView.bounds.size.width > scrollView.contentSize.width)?
+    (scrollView.bounds.size.width - scrollView.contentSize.width) * 0.5 : 0.0;
+    
+    CGFloat offsetY = (scrollView.bounds.size.height > scrollView.contentSize.height)?
+    (scrollView.bounds.size.height - scrollView.contentSize.height) * 0.5 : 0.0;
+    
+    subView.center = CGPointMake(scrollView.contentSize.width * 0.5 + offsetX,
+                                 scrollView.contentSize.height * 0.5 + offsetY);
 }
 
 @end
